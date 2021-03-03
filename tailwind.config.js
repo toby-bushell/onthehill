6fba3541bb2ccb586156cde70479135afc4483a8
toby <toby.bushell@gmail.com>
2021-03-03 22:35:38 +0000

    git first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#3a91ee',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
